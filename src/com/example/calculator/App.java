3f7519be9ba1b40d879007b088ba457c72e3c7f3
YejinY00n <yoon17710@gmail.com>
2025-02-26 13:08:02 +0900

    Feat: App main 동작 추가

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -1,7 +1,25 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Parser parser = new Parser();
+    Calculator cal = new Calculator();
 
+    System.out.println("==== Java Calculator ====");
+
+    while(true) {
+      String expression = sc.nextLine();
+
+      if(expression.equals("exit")) {
+        break;
+      }
+      else {
+        // Parser 통해 분리
+        // Calculator 통해 계산
+      }
+    }
   }
 }
