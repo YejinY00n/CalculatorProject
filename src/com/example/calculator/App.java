5889f319e3a02e57a760c5c2cbe0ef9c79dfbadb
YejinY00n <yoon17710@gmail.com>
2025-02-26 13:27:13 +0900

    Feat: App 메인 동작

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,21 +5,19 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    Parser parser = new Parser();
     Calculator cal = new Calculator();
+    String input;
 
     System.out.println("==== Java Calculator ====");
 
     while(true) {
       String expression = sc.nextLine();
+      // cal 계산 함수, 리턴 값 받아서 결과 출력
 
-      if(expression.equals("exit")) {
-        break;
-      }
-      else {
-        // Parser 통해 분리
-        // Calculator 통해 계산
-      }
+      // exit 입력 시 종료
+      System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+      input = sc.nextLine();
+      if(input.equals("exit")) break;
     }
   }
 }
