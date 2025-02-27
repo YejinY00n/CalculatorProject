6839e47a13d1ae2953adde0755e40b1147fddb06
YejinY00n <yoon17710@gmail.com>
2025-02-27 15:25:47 +0900

    Refactor: 입력 받는 부분 App으로 이동

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,17 +5,39 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    Calculator cal = new Calculator();
+    ArithmeticCalculator cal = new ArithmeticCalculator();
     String input;
 
     System.out.println("==== Java Calculator ====");
 
     while(true) {
-      String expression = sc.nextLine();
-      // cal 계산 함수, 리턴 값 받아서 결과 출력
+      // 입력 받기
+      try {
+        System.out.print("첫번째 숫자: ");   // Memo: 예외 처리
+        int op1 = sc.nextInt();
+        System.out.print("연산자: ");
+        int operator = sc.next().charAt(0);
+        System.out.print("두번째 숫자: ");
+        int op2 = sc.nextInt();
+      }
+      catch (Exception e) {
+        System.out.println("잘못된 입력 값 입니다.");
+        continue;
+      }
+
+      try {
+        cal.calculate();    // 입력받은 식 계산
+      }
+      catch (Exception e) {
+        System.out.println("잘못된 연산입니다.");
+        continue;
+      }
+
+      System.out.printf("계산 결과: %d\n\n", cal.getLastResult());   // 결과 출력
+      cal.getBiggerResults(cal.getLastResult());                   // 현재 결과보다 큰 이전의 계산 결과 출력
 
       // exit 입력 시 종료
-      System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+      System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
       input = sc.nextLine();
       if(input.equals("exit")) break;
     }
