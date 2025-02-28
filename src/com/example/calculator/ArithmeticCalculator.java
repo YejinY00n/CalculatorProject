1983dab5290426fe61e5f3a70fc6dc8677f12680
YejinY00n <yoon17710@gmail.com>
2025-02-28 16:01:36 +0900

    Refactor: this 삭제

--- src/com/example/calculator/ArithmeticCalculator.java
+++ src/com/example/calculator/ArithmeticCalculator.java
@@ -28,23 +28,23 @@ public class ArithmeticCalculator <T extends Number> {
   // 입력 받은 식 계산 후 필드에 결과 저장
   public void calculate() throws Exception {
     // 연산 후 결과 저장
-    if (this.operator == Operator.ADD.getOp()) {
-      this.resultList.add(this.op1.doubleValue() + this.op2.doubleValue());
+    if (operator == Operator.ADD.getOp()) {
+      resultList.add(op1.doubleValue() + op2.doubleValue());
     }
-    else if (this.operator ==  Operator.SUBTRACT.getOp()) {
-      this.resultList.add(this.op1.doubleValue() - this.op2.doubleValue());
+    else if (operator ==  Operator.SUBTRACT.getOp()) {
+      resultList.add(op1.doubleValue() - op2.doubleValue());
     }
-    else if (this.operator ==  Operator.MULTIPLE.getOp()) {
-      this.resultList.add(this.op1.doubleValue() * this.op2.doubleValue());
+    else if (operator ==  Operator.MULTIPLE.getOp()) {
+      resultList.add(op1.doubleValue() * op2.doubleValue());
     }
-    else if (this.operator == Operator.DIVIDE.getOp()) {
+    else if (operator == Operator.DIVIDE.getOp()) {
       // 0으로 나누는 경우
-      if(this.op2.doubleValue() == 0) {
+      if(op2.doubleValue() == 0) {
         System.out.println("0 으로 나눌 수 없습니다.");
         throw new ArithmeticException();
       }
 
-      this.resultList.add(this.op1.doubleValue() / this.op2.doubleValue());
+      this.resultList.add(op1.doubleValue() / op2.doubleValue());
     }
     else {
       System.out.println("유효하지 않은 연산자 입니다.");
@@ -70,14 +70,14 @@ public class ArithmeticCalculator <T extends Number> {
 
   // 마지막 연산 결과 getter
   public T getLastResult() {
-    return (T) this.resultList.get(resultList.size()-1);
+    return (T)resultList.get(resultList.size()-1);
   }
 
   // 저장된 연산 결과들 중 Scanner 로 입력받은 값보다 큰 결과값들을 출력
   public void getBiggerResults(T result) {
     System.out.print(result + " 보다 큰 이전의 계산 결과들: ");
       // 저장된 이전 결과들 중에서 result 보다 큰 값을 출력
-     this.resultList.stream()
+     resultList.stream()
         .filter(r -> (result.doubleValue() - (Double)r < 0))    // 저장된 값 (r) 이 result 보다 크다면
         .forEach(r -> System.out.print(r+", "));
     System.out.println(" ");  // 줄바꿈
